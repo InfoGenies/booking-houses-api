3fc9ccae17bc0374295a1194099e12e21e353b22
InfoGenie <asma.hami@univ-constantine.dz>
2023-06-04 14:30:29 +0100

    first commit

--- api/routes/house.js
+++ api/routes/house.js
@@ -6,13 +6,51 @@ const checkAuth = require('../middleware/check-auth')
 const router = express.Router()
 
 
+const path = require('path')
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        // we use this line path.join to specify the real path automaticly
+      
+        let tempraryImageDirectory;
+
+if (process.env.DEV && process.env.DEV === 'Yes') {
+    tempraryImageDirectory = path.join(__dirname, `../../tmp/`);
+  } else {
+    tempraryImageDirectory = '/tmp/';
+  }
+  
+        cb(null, tempraryImageDirectory);
+    },
+    filename: function(req, file, cb) {
+        const date = new Date().toISOString().replace(/:/g, '-');
+
+        cb(null, date + '-' + file.originalname);
+    }
+});
+const filterFile = (req, file, cb)=>{
+    if(file.mimetype === "image/png" || file.mimetype === "image/jpeg" ){
+        // we give the permesion to store file(image that is png)
+        cb(null,true)  
+    }else{
+        cb(null,false); 
+    }
+}
+
+const upload =  multer({storage : storage , limits: {
+    // we make this to prevent store a big size file(image)
+    fileSize: 1024*1024*5 // 1 MB in bytes
+  },
+  fileFilter : filterFile
+})
+
+
 
 
 router.post('/create_house',checkAuth,HouseController.create_house)
 
 router.post('/create_municipality',HouseController.create_municipality)
 
-router.post('/create_city',HouseController.create_city)
+router.post('/create_city',upload.single('picture'),HouseController.create_city)
 router.get('/city',HouseController.get_city)
 
 
